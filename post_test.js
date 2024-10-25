3808d31394fbf1651cc7cf16f05772eb9b88cbf7
kheder <khdrhswn32@gmail.com>
2024-10-25 23:55:07 +0300

    K6 , testing scenario

---
+++ post_test.js
@@ -0,0 +1,33 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+    scenarios: {
+        constant_request_rate: {
+            executor: 'constant-arrival-rate',
+            rate: 100, // 100 requests total
+            timeUnit: '1m', // in 1 minute
+            duration: '1m', // test duration
+            preAllocatedVUs: 10, // number of virtual users to allocate
+            maxVUs: 20, // max number of virtual users allowed
+        },
+    },
+};
+
+export default function () {
+    const url = 'http://localhost:8000/';
+    const payload = "TaskWriteOnFile&testing_write_on_the";
+    
+    const params = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    };
+
+    const res = http.post(url, payload, params);
+    
+    // Check if the response status is 200
+    check(res, {
+        'is status 200': (r) => r.status === 200,
+    });
+}
